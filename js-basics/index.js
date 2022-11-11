8c08468ca2a9b56fbd954898ccdb9f760b5cfb4b
robertsarafie <sarafierobert@gmail.com>
2022-11-11 15:52:28 +0200

    js-monkey

---
+++ js-basics/index.js
@@ -0,0 +1,56 @@
+// let name = 'Robert';
+// console.log(name);
+
+// // Cannot be a reserved keyword
+// // Should be meaningful
+// // Cannot start with a number (1name)
+// // Cannot contain a space or hyphen (-)
+// // Are case-sensitive
+
+// let firstName = 'Robert';
+// lastName = 'Sarafie';
+
+// !!Constants
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// !!Primitive Types
+// let name = 'Robert'; // String Literal
+// let age = 22; // Number Literal
+// let isApproved = false; // Boolean Literal
+// let firstName = undefined;
+// let selectedColor = null;
+
+// // !!Objects
+// let person = {
+//   name: 'Robert',
+//   age: 22,
+// };
+
+// // Dot Notation
+// person.name = 'John';
+
+// // Bracket Notation
+// let selection = 'name';
+// person[selection] = 'Mary';
+
+// console.log(person.name);
+
+// !!Arrays
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+// !!Functions
+// Performin a task
+function greet(name, lastName) {
+  console.log('Hello ' + name + '' + lastName);
+}
+
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+
+console.log(square(2));
